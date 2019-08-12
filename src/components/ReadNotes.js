caf8b316c494c9000cfb165266f3882b153663c7
Richard Lee <richard.lee@aecom.com>
2019-08-12 16:02:45 -0600

    Added state for type of sort

--- src/components/ReadNotes.js
+++ src/components/ReadNotes.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-function ReadNotes {
+function ReadNotes(props) {
+  let [sortBy, setSortBy] = React.useState("");
+
   return (
     <div>
       <h3>Read Notes</h3>
+      {sortBy === "date" ? <h4>By Date</h4> : ""}
     </div>
-  )
+  );
 }
 
 export default ReadNotes;
