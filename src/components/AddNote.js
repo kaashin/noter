5a801e2be58b98635d0f05a89014fb9aad321400
Richard Lee <richard.lee@aecom.com>
2019-08-12 16:01:36 -0600

    Repathed config source

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { db } from "../config/firebase";
+import { db } from "../config/config";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Editor from "react-simple-code-editor";
