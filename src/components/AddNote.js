4e81087eae3feab20f5dea4ba734e8b7b7a3710e
Richard Lee <richard.lee@aecom.com>
2019-08-14 16:42:40 -0600

    added editor js for input

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,53 +1,61 @@
 import React, { Component } from "react";
 import { db } from "../config/config";
-import TextField from "@material-ui/core/TextField";
+import EditorJs from "react-editor-js";
 import Button from "@material-ui/core/Button";
-import Editor from "react-simple-code-editor";
-import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-javascript";
 
-function AddNote() {
-  const [inputText, setInputText] = React.useState("");
-  return (
-    <div>
-      <form>
-        <div style={{ border: "solid 1px blue" }}>
-          <Editor
-            value={inputText}
-            onValueChange={code => setInputText(code)}
-            highlight={code => highlight(code, languages.js)}
-            padding={10}
-            style={{
-              fontFamily: '"Fira code", "Fira Mono", monospace',
-              fontSize: 12
-            }}
-          />
-        </div>
-        <Button
-          color="primary"
-          size="medium"
-          variant="contained"
-          onClick={() => {
-            db.collection("notes")
-              .add({
-                text: inputText,
-                dateCreated: Date()
-              })
-              .then(docRef => {
-                setInputText("");
-                console.log(`Document written with ID: ${docRef.id}`);
-              })
-              .catch(error => {
-                console.error(`Error adding document: ${error}`);
-              });
-          }}
-        >
-          Add
-        </Button>
-      </form>
-    </div>
-  );
+class AddNote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputData: {}
+    };
+    this.handleSave = this.handleSave.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+  }
+  componentDidMount() {
+    console.log(this.editorInstance);
+  }
+  async handleSave() {
+    const savedData = await this.editorInstance.save();
+
+    // Save to DB
+    savedData.blocks.forEach((block, index) => {
+      db.collection("note-blocks").add({
+        data: block.data,
+        time: Date(savedData.time)
+      });
+    });
+    //Clear he editor
+    this.editorInstance.clear();
+  }
+  async handleCheck() {
+    console.log(this.editorInstance);
+  }
+  render() {
+    return (
+      <div>
+        <form>
+          <div style={{ border: "solid 1px blue" }}>
+            <EditorJs
+              instanceRef={instance => (this.editorInstance = instance)}
+              data={this.state.inputData}
+            />
+          </div>
+          <Button
+            color="primary"
+            size="medium"
+            variant="contained"
+            onClick={this.handleSave}
+          >
+            Add
+          </Button>
+          <Button color="secondary" size="medium" onClick={this.handleCheck}>
+            Check
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddNote;
